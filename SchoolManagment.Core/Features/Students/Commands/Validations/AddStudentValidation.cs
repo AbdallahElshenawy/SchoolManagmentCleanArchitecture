484b6c5652450f3c16c3e8b9ceb60a05dc491b45
Abdallah Elshenawy <elshenawya894@gmail.com>
2025-07-21 10:57:18 +0300

    Checks For nullable Department Id

--- SchoolManagment.Core/Features/Students/Commands/Validations/AddStudentValidation.cs
+++ SchoolManagment.Core/Features/Students/Commands/Validations/AddStudentValidation.cs
@@ -1,11 +1,16 @@
 ﻿using FluentValidation;
 using SchoolManagment.Core.Features.Students.Commands.Models;
+using SchoolManagment.Service.Abstracts;
 namespace SchoolManagment.Core.Features.Students.Commands.Validations
 {
     public class AddStudentValidation : AbstractValidator<AddStudentCommand>
     {
-        public AddStudentValidation()
+        private readonly IDepartmentService _departmentService;
+
+        public AddStudentValidation(IDepartmentService departmentService)
         {
+            _departmentService = departmentService;
+
             RuleFor(s => s.Name).NotEmpty().WithMessage("Name can't be empety")
                 .NotNull()
                 .MaximumLength(10);
@@ -13,6 +18,14 @@ namespace SchoolManagment.Core.Features.Students.Commands.Validations
             RuleFor(s => s.Address).NotEmpty().WithMessage("Address can't be empety")
                 .NotNull()
                 .MaximumLength(10);
+            RuleFor(s => s.DepartmentId)
+                .MustAsync(async (id, cancellation) =>
+                {
+                    if (id is null) return true;
+                    var department = await _departmentService.GetDepartmentByIdAsync(id.Value);
+                    return department != null;
+                })
+                .WithMessage("Department not found");
         }
     }
 }
