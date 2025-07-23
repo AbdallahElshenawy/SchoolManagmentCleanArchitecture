e7957f94e446093a810bb21c2b5b9c012ed65789
Abdallah Elshenawy <elshenawya894@gmail.com>
2025-07-23 10:49:03 +0300

    Added ChangeUserPassword Endpoint

---
+++ SchoolManagment.Core/Features/Users/Commands/Validations/ChangeUserPasswordValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using SchoolManagment.Core.Features.Users.Commands.Models;
+using SchoolManagment.Core.Resources;
+namespace SchoolManagment.Core.Features.Users.Commands.Validations
+{
+    public class ChangeUserPasswordValidator : AbstractValidator<ChangeUserPasswordCommand>
+    {
+        public ChangeUserPasswordValidator(IStringLocalizer<SharedResources> localizer)
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage(localizer[SharedResourcesKeys.NotEmpty])
+                .NotNull().WithMessage(localizer[SharedResourcesKeys.Required]);
+
+            RuleFor(x => x.CurrentPassword)
+                     .NotEmpty().WithMessage(localizer[SharedResourcesKeys.NotEmpty])
+                     .NotNull().WithMessage(localizer[SharedResourcesKeys.Required]);
+            RuleFor(x => x.NewPassword)
+                     .NotEmpty().WithMessage(localizer[SharedResourcesKeys.NotEmpty])
+                     .NotNull().WithMessage(localizer[SharedResourcesKeys.Required]);
+            RuleFor(x => x.ConfirmPassword)
+             .Equal(x => x.NewPassword).WithMessage("Passwords do not match.");
+
+        }
+    }
+}
